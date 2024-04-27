2984d173a48c72806676af8458ca621abd691a5d
Jiří Setnička <setnicka@seznam.cz>
2024-04-27 23:11:59 +0200

    Web: Templaty + static CSS/JS files

--- static/app.js
+++ static/app.js
@@ -1,3 +1,10 @@
+function bootstrap_alert(el, type, msg) {
+    el.innerHTML = '<div class="alert alert-'+type+' alert-dismissible fade show" role="alert">'
+        + msg
+        + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>'
+    + '</div>';
+}
+
 // Elements
 var canvas;
 var canvasContext;
