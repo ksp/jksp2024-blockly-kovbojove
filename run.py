14f468e7e34973e99086191effd9651826b0c546
Jiří Setnička <setnicka@seznam.cz>
2024-04-30 21:12:50 +0200

    Run: automatické vytváření složek

--- run.py
+++ run.py
@@ -2,11 +2,18 @@
 
 import glob
 import sys
+from pathlib import Path
 
 import blockly.web
 import blockly.game
 from blockly.map import GameMap
-from blockly.team import Team
+from blockly.team import Team, data_dir as teams_dir
+
+save_dir = "save"
+
+# Ensure directories exists
+Path(save_dir).mkdir(parents=True, exist_ok=True)
+Path(teams_dir).mkdir(parents=True, exist_ok=True)
 
 teams = [
     Team("red", "steamCrazyHorse"),
@@ -16,7 +23,7 @@ teams = [
 ]
 
 load_from_file = None
-save_files = glob.glob("save/save_*.json")
+save_files = glob.glob(f"{save_dir}/save_*.json")
 if len(save_files) > 0:
     load_from_file = sorted(save_files)[-1]
     print(f"Loading game from file '{load_from_file}'")
@@ -24,7 +31,8 @@ if len(save_files) > 0:
 game_map = GameMap(width=40, height=40, teams=teams,
                    cowboys_per_team=4,
                    gold_count=5,
-                   load_from_file=load_from_file)
+                   load_from_file=load_from_file,
+                   save_dir=save_dir)
 
 blockly.game.G = blockly.game.Game(teams=teams, map=game_map, org_login="org", org_passwd="org")
 
